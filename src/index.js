7edab6bdb41c00396822db316ef3a9f655a3ad2a
novikov-al-st <novikov.al.st@gmail.com>
2023-01-19 08:53:35 +0200

    feat: implement method.

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let symbolCodes = [];
+
+    for (let i = 0; i < expr.length / 10; i++) {
+        symbolCodes.push(expr.substring(i * 10, i * 10 + 10));
+    }
+
+    const resultArray = symbolCodes.map( (value) =>
+    {
+        if(value.substring(0,2) === '**'){
+            return ' ';
+        } else {
+            let literalCodes = [];
+            for (let k = 0; k < value.length / 2; k++) {
+                literalCodes.push(value.substring(k * 2, k * 2 + 2));
+            }
+
+            return MORSE_TABLE[literalCodes.map( (code) => code === '00' ? '' : code === '11' ? '-' : '.').join('')];
+        }
+    });
+    
+    return resultArray.join('');
 }
 
 module.exports = {
